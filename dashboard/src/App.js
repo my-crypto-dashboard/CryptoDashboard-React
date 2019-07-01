7a708259e53b3551f388daa1efd78a416b80d568
Chiboba <briangfelix@gmail.com>
2019-07-01 14:18:31 -0700

    Testing firebase

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.scss';
 import HeaderNav from './HeaderNav.js';
+import fire from './config/firebase';
 
-function App() {
-  return (
+class App extends Component {
 
-    <div className="App">
-      <HeaderNav />
-    </div>
-  );
+  consturctor() {
+    super();
+    this.states = {
+      user: null
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <HeaderNav />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
