64530cb49069f9902c1f8a921fda10e45206e547
ayunas <ayunas@gmail.com>
2019-07-01 12:23:50 -0500

    created header nav

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="#" className="App-logo" alt="logo" />
         <p>
           <code>My Crypto Dashboard</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <a href='#'>Dashboard</a>
+          <a href='#'>Favorites</a>
+          <a href='#'>Wallets</a>
+          <a href='#'>About</a>
+          <a href='#'>Logout</a>
+        </nav>
       </header>
     </div>
   );
