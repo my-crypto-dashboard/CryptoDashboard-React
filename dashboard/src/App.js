7225222bbdcc42592f54cf4d9a643be09acc55f1
ayunas <ayunas@gmail.com>
2019-07-01 12:53:58 -0500

    created <HeaderNav/> component and placed all header code in the component

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import './App.scss';
+import HeaderNav from './HeaderNav.js';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src="#" className="App-logo" alt="logo" />
-        <p>
-          <code>My Crypto Dashboard</code>
-        </p>
-        <nav>
-          <a href='#'>Dashboard</a>
-          <a href='#'>Favorites</a>
-          <a href='#'>Wallets</a>
-          <a href='#'>About</a>
-          <a href='#'>Logout</a>
-        </nav>
-      </header>
+      <HeaderNav />
     </div>
   );
 }
