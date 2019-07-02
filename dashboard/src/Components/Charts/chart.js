b68d5a396a2301b6ab9a50300f8479dd6537cb23
Chiboba <briangfelix@gmail.com>
2019-07-02 14:56:21 -0700

    Fix to the login situation

--- dashboard/src/Components/Charts/chart.js
+++ dashboard/src/Components/Charts/chart.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 const Chart = props => {
 
   useEffect(() => {
-    axios.get(`https://api.coingecko.com/api/v3/coins//market_chart?vs_currency=usd&days=${30}`)
+    axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=${364}`)
     .then(res => {
         let priceData = res.data.prices;
         let marketCapData = res.data.market_caps;
@@ -29,43 +29,48 @@ const Chart = props => {
         let maxDate = priceData[priceData.length -1][0];
         let minPrice = d3.min(priceData, function(d) { return d[1]});
         let maxPrice = d3.max(priceData, function(d) { return d[1]});
+        let diffPrice = ((maxPrice - minPrice) / 2);
+
+        let minMC = d3.min(marketCapData, function(d) { return d[1]});
+        let maxMC = d3.max(marketCapData, function(d) { return d[1]});
+        let changeMC = (maxMC % 4)
         
-        let y = d3.scaleLinear().rangeRound([height, 0]);
-        let x = d3.scaleTime().rangeRound([0, width]);  
+        let y = d3.scaleLinear().rangeRound([height, 0])
+        let x = d3.scaleTime().rangeRound([0, width]);
         
         let y2 = d3.scaleLinear().rangeRound([height, 0]);
+        console.log(minPrice -diffPrice);
+        console.log(maxPrice + maxPrice);
 
         let line = d3.line().x(function(d) {return x(d[0])}).y(function(d) { return y(d[1])})
-                            x.domain([minDate, maxDate])
+                            x.domain([minDate, maxDate]).tickFormat(d3.timeFormat("%d%m"));
                             y.domain([minPrice, maxPrice]);
 
         let marketCapLine = d3.line().x(function(d) {return x(d[0])}).y(function(d) {return y2(d[1])})
-                            y2.domain(d3.extent(marketCapData, function(d) { return d[1]}));
+                            y2.domain([minMC - changeMC, maxMC + changeMC])
 
 
         g.append('g')
             .attr('transform', `translate(0, ${height})`)
-            .call(d3.axisBottom(x))
+            .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%d. %b")))
             .select('.domain')
             .remove();
 
         g.append('g')
-            .call(d3.axisLeft(y2))
+            .call(d3.axisLeft(y2).ticks(4))
             .append('text')
             .attr('fill', '#000')
-            .attr('transform', 'rotate(-90')
-            .attr('y', 6)
+            .attr('transform', 'rotate(-90)')   
             .attr('dy', '0.71em')
             .attr('text-anchor', 'end')
             .text('Martket Cap');
 
         g.append('g')
             .attr('transform', `translate(${width}, 0)`)
-            .call(d3.axisRight(y))
+            .call(d3.axisRight(y).ticks(4))
             .append('text')
             .attr('fill', '#000')
             .attr('transform', 'rotate(-90')
-            .attr('y', 6)
             .attr('dy', '0.71em')
             .attr('text-anchor', 'end')
             .text('Price ($)');
