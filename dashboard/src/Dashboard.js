9ae1abfac8232094391707ac293627867da6706a
ayunas <ayunas@gmail.com>
2019-07-01 20:46:21 -0500

    search and crypto lists are functional, rendering asset name and price for each click or search

--- dashboard/src/Dashboard.js
+++ dashboard/src/Dashboard.js
@@ -11,9 +11,7 @@ class Dashboard extends React.Component {
         this.state = {
             coins: []
         }
-
     }
-
 
     componentDidMount() {
         axios.get('https://api.coingecko.com/api/v3/coins/list')
@@ -33,35 +31,93 @@ class Dashboard extends React.Component {
             .catch(err => console.log(err));
     }
 
-    cryptoSearch(crypto) {
+
+    displayCryptoLeft = coin => {
+        axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin.id}&vs_currencies=usd`)
+            .then(res => {
+                console.log(res.data);
+                this.setState({ cryptoLeft: res.data })
+            })
+            .catch(err => console.log(err));
+    }
+
+    displayCryptoRight = coin => {
+        axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin.id}&vs_currencies=usd`)
+            .then(res => {
+                console.log(res.data);
+                this.setState({ cryptoRight: res.data })
+            })
+            .catch(err => console.log(err));
+    }
+
+    cryptoSearch(crypto, form) {
         console.log('cryptosearch triggered');
 
-        const foundCrypto = this.state.coins.filter(coin => coin.symbol === crypto.value);
-        this.setState({ foundCrypto: foundCrypto })
-        if (foundCrypto.length === 0) {
-            console.log('no crypto ticker found with symbol: ', crypto);
-        } else {
-            this.showCrypto(foundCrypto[0]);
+        if (form.name === 'left-form') {
+            const cryptoResult1 = this.state.coins.filter(coin => coin.symbol === crypto.value);
+            this.setState({ cryptoResult1 });
+            if (cryptoResult1.length === 0) {
+                alert('no crypto ticker found with that symbol. ')
+            } else {
+                this.displayCryptoLeft(cryptoResult1[0])
+            }
+        } else if (form.name === 'right-form') {
+            const cryptoResult2 = this.state.coins.filter(coin => coin.symbol === crypto.value);
+            this.setState({ cryptoResult2 })
+            if (cryptoResult2.length === 0) {
+                alert('no crypto ticker found with that symbol. ', crypto.value);
+            } else {
+                this.displayCryptoRight(cryptoResult2[0]);
+            }
         }
     }
 
     render() {
         return (
-            <div className='coins'>
-                <form onSubmit={(e) => {
-                    e.preventDefault();
-                    this.cryptoSearch(e.target[0]);
-                }}>
-                    <input name="cryptoTicker" placeholder="Crypto Ticker Symbol" />
-                </form>
-                {this.state.foundCrypto && <div>{this.state.foundCrypto[0].name}</div>}
+            <>
+                <div className='forms'>
+                    <form name='left-form' onSubmit={(e) => {
+                        e.preventDefault();
+                        this.cryptoSearch(e.target[0], e.target);
+                    }}>
+                        <input name="cryptoTicker" placeholder="Crypto Ticker Symbol" />
+                    </form>
 
-                {this.state.crypto && (<div>
-                    <div>{Object.keys(this.state.crypto)[0]}</div>
-                    <div>{Object.values(this.state.crypto)[0]['usd']}</div>
-                </div>)}
-                {this.state.coins.map((coin, i) => <div key={i} className="coin" onClick={() => this.props.showCrypto(coin)}>{coin.symbol}</div>)}
-            </div>
+                    <form name='right-form' onSubmit={(e) => {
+                        e.preventDefault();
+                        this.cryptoSearch(e.target[0], e.target);
+                    }}>
+                        <input name="cryptoTicker" placeholder="Crypto Ticker Symbol" />
+                    </form>
+                </div>
+
+                <main>
+                    <div className="data">
+                        {this.state.cryptoLeft && (<div>
+                            <div>{Object.keys(this.state.cryptoLeft)[0]}</div>
+                            <div>${Object.values(this.state.cryptoLeft)[0]['usd']} usd</div>
+                        </div>)}
+
+                        {this.state.crypto && (<div>
+                            <div>{Object.keys(this.state.crypto)[0]}</div>
+                            <div>${Object.values(this.state.crypto)[0]['usd']} usd</div>
+                        </div>)}
+
+                        {this.state.cryptoRight && (<div>
+                            <div>{Object.keys(this.state.cryptoRight)[0]}</div>
+                            <div>${Object.values(this.state.cryptoRight)[0]['usd']} usd</div>
+                        </div>)}
+                    </div>
+                    <div className='coins'>
+                        <div className='leftCoins'>
+                            {this.state.coins.map((coin, i) => <div key={i} className="left-coin" onClick={() => this.displayCryptoLeft(coin)}>{coin.symbol}</div>)}
+                        </div>
+                        <div className='rightCoins'>
+                            {this.state.coins.map((coin, i) => <div key={i} className="right-coin" onClick={() => this.displayCryptoRight(coin)}>{coin.symbol}</div>)}
+                        </div>
+                    </div>
+                </main>
+            </>
         )
     }
 }
