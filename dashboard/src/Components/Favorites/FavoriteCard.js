e22cee64286d79f75d788ad66bb32660386711fc
Brandon Allison <brandona8128@outlook.com>
2019-07-02 14:40:26 -0400

    WIP: puilling in data for 7 days of prices

--- dashboard/src/Components/Favorites/FavoriteCard.js
+++ dashboard/src/Components/Favorites/FavoriteCard.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
 import '../components.scss'
 
 
 
 
-const FavoriteCard = props => {
-    console.log('props', props)
-  return (
-      
-    <div className={'card-1'}>  
-      <h2>{props.name}</h2>
-      <h4>{props.price}</h4>
-      <h4>{props.mCap}</h4>
-      <h5>{props.change}</h5>
+class FavoriteCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     
+    };
+  }
+    componentDidMount(){
+      let ids = this.state.ids
+      console.log(ids)
+      axios
+      .get(`https://api.coingecko.com/api/v3/coins/${this.props.name}/market_chart?vs_currency=usd&days=7`)
+      .then (response => {
+        console.log('response in card', this.props.name, response.data.prices)
+        })
+      .catch(err => {
+          console.log(err);
+      })
+  };
+
+    
+    render (){
+      return(
+      <div className={'card-1'}>  
+      <p className={'header'}>{this.props.name.toUpperCase()}</p>
+      <p>24hr. Change:   {this.props.change}</p>
+      <p></p>
+      <p>Current Price:   ${this.props.price}</p>
+      <p>Market Cap:   {this.props.mCap}</p>
     </div>
-  );
+      );
+
+      }
+
+      
+   
 };
 
 
