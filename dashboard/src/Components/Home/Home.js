175a0ae2288de52f21b600bd067f701ee25bca4a
Brandon Allison <brandona8128@outlook.com>
2019-07-03 00:59:47 -0400

    WIP:added to home component, styling done

--- dashboard/src/Components/Home/Home.js
+++ dashboard/src/Components/Home/Home.js
@@ -14,6 +14,7 @@ return (
     <div  style={{display:'flex', flexDirection: 'column', alignItems:'center'}}>
         <img src={logo} style={{ marginTop: '100px',height: '250px'}}/>
         <button onClick={update} style={{width:'300px', padding: '20px 0', fontSize:'25px'}}>Get Started</button>
+        <p style={{color:"#57b7bd"}}><i>"The future of money is digital currency" - Bill Gates</i></p>
     </div>
 )
 
