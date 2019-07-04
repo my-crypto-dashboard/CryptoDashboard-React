b36465ca5b28e5a343aa81e2d50769fad2460cd8
ItelSunday <iteldomingotech@gmail.com>
2019-07-03 17:56:25 -0700

    refactored link tags

--- dashboard/src/Components/About/About.js
+++ dashboard/src/Components/About/About.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {NavLink} from 'react-router-dom'
+import {NavLink} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import '../About/About.scss'
 import Grid from '@material-ui/core/Grid';
@@ -49,30 +50,56 @@ The market prices can be paired off with your choice and make a list of prices a
             <div className="image-box">
               <img className="avatar" src="https://avatars1.githubusercontent.com/u/47340734?s=460&v=4" alt="" style={{width: "200px"}}/>
               <p>Brandon Allison</p>
-              <img className="icon-in" href="https://www.linkedin.com/in/brandon-allison-129987124/" src={iconLinkedin} style={{width: "24px"}} />
-              <img className="icon-gh" href="https://github.com/orgs/my-crypto-dashboard/people/BrandonJAllison" src={iconGithub} style={{width: "24px"}} />
+
+              <a href="https://www.linkedin.com/in/brandon-allison-129987124/">
+              <img src={iconLinkedin} className="icon-in"  style={{width: "24px"}} />
+              </a>
+
+              <a href="https://github.com/orgs/my-crypto-dashboard/people/BrandonJAllison">
+              <img className="icon-gh" src={iconGithub} style={{width: "24px"}} />
+              </a>
             </div>
             
 
             <div className="image-box">
               <img className="avatar" src="https://avatars1.githubusercontent.com/u/35277732?s=460&v=4" alt="" style={{width: "200px"}}/>
               <p>Bryan Garcia-Felix</p>
-              <img className="icon-in" href="https://www.linkedin.com/in/bryan-garciafelix/" src={iconLinkedin} style={{width: "24px"}} />
-              <img className="icon-gh" href="https://github.com/Bryan-GF" src={iconGithub} style={{width: "24px"}} />
+
+              <a href="https://www.linkedin.com/in/bryan-garciafelix/">
+              <img src={iconLinkedin} className="icon-in"  style={{width: "24px"}} />
+              </a>
+
+              <a href="https://github.com/Bryan-GF">
+              <img className="icon-gh" src={iconGithub} style={{width: "24px"}} />
+              </a>
             </div>
 
             <div className="image-box">
               <img className="avatar" src="https://media.licdn.com/dms/image/C5603AQEQrRdEV93fLg/profile-displayphoto-shrink_200_200/0?e=1567641600&v=beta&t=v4kNXBQ9VdE6rHQKinuYDVoH6taTCYGt3PwC60EK5yU" alt="" style={{width: "200px"}}/>
               <p>Itel Domingo</p>
-              <img className="icon-in" href="https://www.linkedin.com/in/iteldomingo" src={iconLinkedin} style={{width: "24px"}} />
-              <img className="icon-gh" href="https://github.com/iteldomingo" src={iconGithub} style={{width: "24px"}} />
+
+              <a href="https://www.linkedin.com/in/iteldomingo">
+              <img src={iconLinkedin} className="icon-in"  style={{width: "24px"}} />
+              </a>
+
+              <a href="https://github.com/iteldomingo">
+              <img className="icon-gh" src={iconGithub} style={{width: "24px"}} />
+              </a>
+
             </div>
 
             <div className="image-box">
               <img className="avatar" src="https://avatars0.githubusercontent.com/u/30554629?s=460&v=4" alt=""style={{width: "200px"}}/>
               <p>Amir Yunas</p>
-              <img className="icon-in" href="#"  src={iconLinkedin} style={{width: "24px"}} />
-              <img className="icon-gh" href="https://github.com/ayunas" src={iconGithub} style={{width: "24px"}} />
+
+              <a href="#">
+              <img src={iconLinkedin} className="icon-in"  style={{width: "24px"}} />
+              </a>
+
+              <a href="https://github.com/ayunas">
+              <img className="icon-gh" src={iconGithub} style={{width: "24px"}} />
+              </a>
+
             </div>
 
           </div>
